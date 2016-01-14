fad105614f0745af71f4ca8df4b74bd67c599cc8
Jonathan Wilson Bruce <jwbmw3@gmail.com>
2016-01-14 10:21:51 -0500

    SodaBible Update on 1/12/16
    Hello!
    
    This is just a quick update that I made to the MyProfile page a few
    days ago. I just added a quick description to it. I will update it more
    in the future, so stay tuned for that!
    
    Thank You for checking out this commit and God Bless!
    
    —Jonathan Bruce (@JwBZx)

---
+++ 04.1MyProfile.js
@@ -0,0 +1,4 @@
+// My Profile [Welcome]
+console.log("My Profile");
+console.log("Welcome to My Profile! This is where you tell the world about yourself,");
+console.log("look at your Notes and Highlights, and more!");
